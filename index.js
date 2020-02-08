e66ae7a531242d2751c43221209c3ea66cb2da73
GamingGuyTyler <gamingguycontact@gmail.com>
2020-02-07 23:40:03 -0800

    Restarted Repo due to Repl Breaking
    So for 2 months, repl.it wasn't letting me making any commits to Github. I waited for a response and all I got was to restart the repo, so here you go. Old commits can be found in the repl: https://repl.it/@TylerFlowers/ggtylerr-website-old

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 // Init Vars
-const express = require('express')
-const app = express()
-var path = require('path')
+const express = require('express');
+const app = express();
+var path = require('path');
 const port = 3000
 
 // Static crap
-app.use(express.static(__dirname, {
-    extensions: ['html', 'htm']
+app.use(express.static('public', {
+  extensions: ['html', 'htm']
 }));
 
 // Index
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.get('/', (req, res) => res.sendFile('public/index.html'))
+
+// Removing extensions (manually, hopefully I can figure something out at a later time)
+app.get('/blog',(req,res) => res.sendFile(path.join(__dirname + '/public/blog.html')))
+app.get('/blog/01-01-2020',(req,res) => res.sendFile(path.join(__dirname + '/public/01-01-2020.html')))
+app.get('/social',(req,res) => res.sendFile(path.join(__dirname + '/public/social.html')))
+app.get('/projects',(req,res) => res.sendFile(path.join(__dirname + '/public/projects.html')))
+app.get('/projects/released',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/released.html')))
+app.get('/projects/upcoming',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/upcoming.html')))
 
 // Error files
 app.use(function(req,res) {
-  res.status(400)
-  res.sendFile(path.join(__dirname + '/error/404.html'))
+  res.status(404)
+  res.sendFile(path.join(__dirname + '/public/error/404.html'))
+})
+
+app.use(function(req,res) {
+  res.status(403)
+  res.sendFile(path.join(__dirname + '/public/error/403.html'))
+})
+
+app.use(function(req,res) {
+  res.status(408)
+  res.sendFile(path.join(__dirname + '/error/408.html'))
+})
+
+app.use(function(req,res) {
+  res.status(418)
+  res.sendFile(path.join(__dirname + '/error/418.html'))
 })
 
 app.use(function(error,req,res,next) {
@@ -23,5 +46,10 @@ app.use(function(error,req,res,next) {
   res.sendFile(path.join(__dirname + '/error/500.html'))
 })
 
+app.use(function(error,req,res,next) {
+  res.status(503)
+  res.sendFile(path.join(__dirname + '/public/error/503.html'))
+})
+
 // Listen
-app.listen(port)
+app.listen(port, () => console.log('Server has started!'))
