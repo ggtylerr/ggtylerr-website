fe5fee425f937003466cc307c4265f8ee573f8ce
GamingGuyTyler <gamingguycontact@gmail.com>
2020-03-08 20:58:27 -0700

    Fixed up the code a bit (according to JetBrain's WebStorm IDE)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Init Vars
 const express = require('express');
 const app = express();
-var path = require('path');
-const port = 3000
+const path = require('path');
+const port = 3000;
 
 // Static crap
 app.use(express.static('public', {
@@ -10,46 +10,46 @@ app.use(express.static('public', {
 }));
 
 // Index
-app.get('/', (req, res) => res.sendFile('public/index.html'))
+app.get('/', (req, res) => res.sendFile('public/index.html'));
 
 // Removing extensions (manually, hopefully I can figure something out at a later time)
-app.get('/blog',(req,res) => res.sendFile(path.join(__dirname + '/public/blog.html')))
-app.get('/blog/01-01-2020',(req,res) => res.sendFile(path.join(__dirname + '/public/01-01-2020.html')))
-app.get('/social',(req,res) => res.sendFile(path.join(__dirname + '/public/social.html')))
-app.get('/projects',(req,res) => res.sendFile(path.join(__dirname + '/public/projects.html')))
-app.get('/projects/released',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/released.html')))
-app.get('/projects/upcoming',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/upcoming.html')))
+app.get('/blog',(req,res) => res.sendFile(path.join(__dirname + '/public/blog.html')));
+app.get('/blog/01-01-2020',(req,res) => res.sendFile(path.join(__dirname + '/public/01-01-2020.html')));
+app.get('/social',(req,res) => res.sendFile(path.join(__dirname + '/public/social.html')));
+app.get('/projects',(req,res) => res.sendFile(path.join(__dirname + '/public/projects.html')));
+app.get('/projects/released',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/released.html')));
+app.get('/projects/upcoming',(req,res) => res.sendFile(path.join(__dirname + '/public/projects/upcoming.html')));
 
 // Error files
 app.use(function(req,res) {
-  res.status(404)
-  res.sendFile(path.join(__dirname + '/public/error/404.html'))
-})
+  res.status(404);
+  res.sendFile(path.join(__dirname + '/public/error/404.html'));
+});
 
 app.use(function(req,res) {
-  res.status(403)
-  res.sendFile(path.join(__dirname + '/public/error/403.html'))
-})
+  res.status(403);
+  res.sendFile(path.join(__dirname + '/public/error/403.html'));
+});
 
 app.use(function(req,res) {
-  res.status(408)
-  res.sendFile(path.join(__dirname + '/error/408.html'))
-})
+  res.status(408);
+  res.sendFile(path.join(__dirname + '/public/error/408.html'));
+});
 
 app.use(function(req,res) {
-  res.status(418)
-  res.sendFile(path.join(__dirname + '/error/418.html'))
-})
+  res.status(418);
+  res.sendFile(path.join(__dirname + '/public/error/418.html'));
+});
 
-app.use(function(error,req,res,next) {
-  res.status(500)
-  res.sendFile(path.join(__dirname + '/error/500.html'))
-})
+app.use(function(req,res) {
+  res.status(500);
+  res.sendFile(path.join(__dirname + '/public/error/500.html'));
+});
 
-app.use(function(error,req,res,next) {
-  res.status(503)
-  res.sendFile(path.join(__dirname + '/public/error/503.html'))
-})
+app.use(function(req,res) {
+  res.status(503);
+  res.sendFile(path.join(__dirname + '/public/error/503.html'));
+});
 
 // Listen
-app.listen(port, () => console.log('Server has started!'))
+app.listen(port, () => console.log('Server has started!'));
