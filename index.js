110cc29fde0f1f1664a455e8a5a180ca8f3f6c30
Tyler Flowers <ggtylerr_contact@protonmail.com>
2021-02-12 07:36:55 +0000

    Arrow functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const fs = require('fs');
 let publicdir = __dirname + '/public';
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   if (req.path.indexOf('.') === -1) {
     let file = publicdir + req.path + '.html';
     fs.exists(file, function(exists) {
@@ -29,32 +29,32 @@ app.use(express.static(publicdir, {
 app.get('/', (req, res) => res.sendFile('public/index.html'));
 
 // Error files
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(404);
   res.sendFile(path.join(__dirname + '/public/error/404.html'));
 });
 
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(403);
   res.sendFile(path.join(__dirname + '/public/error/403.html'));
 });
 
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(408);
   res.sendFile(path.join(__dirname + '/public/error/408.html'));
 });
 
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(418);
   res.sendFile(path.join(__dirname + '/public/error/418.html'));
 });
 
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(500);
   res.sendFile(path.join(__dirname + '/public/error/500.html'));
 });
 
-app.use(function(req,res) {
+app.use((req,res) => {
   res.status(503);
   res.sendFile(path.join(__dirname + '/public/error/503.html'));
 });
